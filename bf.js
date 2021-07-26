8c959cdabc9c5d761b247d4ae0294cec5e77c29d
Sarthik Garg <sarthikgarg01@gmail.com>
2021-07-26 18:37:35 +0530

    Added all the operators other than [] and working fine currently

--- bf.js
+++ bf.js
@@ -1,7 +1,12 @@
-function brainfuck(str) {
+const chalk = require("chalk");
+const index = chalk.bold.blue;
+const error = chalk.bold.red;
+
+function brainfuck(str = "", input = "") {
 	const tape = [...Array(1024).fill(0)];
 	const brackets = [];
-	let pointer = 0;
+	let tpointer = 0;
+	let ipointer = 0;
 
 	let ix = 0;
 	let isIncrement = true;
@@ -12,11 +17,39 @@ function brainfuck(str) {
 		}
 		switch (str[ix]) {
 			case ">":
-				if (pointer < tape.length) pointer++;
+				if (tpointer < tape.length - 1) tpointer++;
+				else {
+					const err = str.substring(ix - 3, ix + 3);
+					throw new Error("Tape length exceeded at index" + index(ix) + ": " + error(err));
+				}
 				break;
+			case "<":
+				if (tpointer > 0) tpointer--;
+				else {
+					const err = str.substring(ix - 3, ix + 3);
+					throw new Error("Tape length subceeded at index" + index(ix) + ": " + error(err));
+				}
+				break;
+			case "+":
+				if (tape[tpointer] < 255) tape[tpointer]++;
+				else tape[tpointer] = 0;
+				break;
+			case "-":
+				if (tape[tpointer] > 0) tape[tpointer]--;
+				else tape[tpointer] = 255;
+				break;
+			case ".":
+				process.stdout.write(String.fromCharCode(tape[tpointer]));
+				break;
+			case ",":
+				if (ipointer < input.length) tape[tpointer] = input[ipointer++].charCodeAt(0);
+				else {
+					const err = str.substring(ix - 3, ix + 3);
+					throw new Error("No input char at index" + index(ix) + ": " + error(err));
+				}
 		}
 		ix++;
 	}
 }
 
-brainfuck("hello");
+brainfuck("+++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++.<.");
