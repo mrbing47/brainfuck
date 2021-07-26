7e5069b427c5f035ac3341e1b50d376e4b7e73a9
Sarthik Garg <sarthikgarg01@gmail.com>
2021-07-26 20:26:31 +0530

    - Added the loop. - Added some options to give input, initialise tape, tape size and wrap BOOLEAN.

--- bf.js
+++ bf.js
@@ -1,42 +1,72 @@
 const chalk = require("chalk");
 const index = chalk.bold.blue;
 const error = chalk.bold.red;
+const err_length = 5;
+class Stack {
+	constructor() {
+		this.stack = [];
+	}
 
-function brainfuck(str = "", input = "") {
-	const tape = [...Array(1024).fill(0)];
-	const brackets = [];
+	push(item) {
+		this.stack.push(item);
+	}
+
+	pop() {
+		if (this.stack.length == 0) return "Underflow";
+		return this.stack.pop();
+	}
+
+	isEmpty() {
+		return this.stack.length === 0 ? true : false;
+	}
+}
+
+function brainfuck(str = "", { input = "", init = [], size = 256, wrap = false } = {}) {
+	const tape = [...init, ...Array(size - init.length).fill(0)];
+	const brackets = new Stack();
 	let tpointer = 0;
 	let ipointer = 0;
 
 	let ix = 0;
-	let isIncrement = true;
+	let isLoopFail = false;
 	while (ix < str.length && ix >= 0) {
-		if (!isIncrement) {
-			ix--;
-			continue;
+		if (isLoopFail) {
+			ix++;
+			if (str[ix - 1] === "]") isLoopFail = false;
+			else continue;
 		}
 		switch (str[ix]) {
 			case ">":
 				if (tpointer < tape.length - 1) tpointer++;
 				else {
-					const err = str.substring(ix - 3, ix + 3);
-					throw new Error("Tape length exceeded at index" + index(ix) + ": " + error(err));
+					if (!wrap) {
+						const err1 = str.substring(ix - err_length, ix);
+						const err2 = str.substring(ix + 1, ix + err_length);
+						throw new Error(
+							"Tape length exceeded at index " + index(ix) + " : " + err1 + error(str[ix]) + err2
+						);
+					} else tpointer = 0;
 				}
 				break;
 			case "<":
 				if (tpointer > 0) tpointer--;
 				else {
-					const err = str.substring(ix - 3, ix + 3);
-					throw new Error("Tape length subceeded at index" + index(ix) + ": " + error(err));
+					if (!wrap) {
+						const err1 = str.substring(ix - err_length, ix);
+						const err2 = str.substring(ix + 1, ix + err_length);
+						throw new Error(
+							"Tape length subceeded at index " + index(ix) + " : " + err1 + error(str[ix]) + err2
+						);
+					} else tpointer = tape.length - 1;
 				}
 				break;
 			case "+":
-				if (tape[tpointer] < 255) tape[tpointer]++;
-				else tape[tpointer] = 0;
+				if (tape[tpointer] < 127) tape[tpointer]++;
+				else tape[tpointer] = -128;
 				break;
 			case "-":
-				if (tape[tpointer] > 0) tape[tpointer]--;
-				else tape[tpointer] = 255;
+				if (tape[tpointer] > -128) tape[tpointer]--;
+				else tape[tpointer] = 127;
 				break;
 			case ".":
 				process.stdout.write(String.fromCharCode(tape[tpointer]));
@@ -44,12 +74,31 @@ function brainfuck(str = "", input = "") {
 			case ",":
 				if (ipointer < input.length) tape[tpointer] = input[ipointer++].charCodeAt(0);
 				else {
-					const err = str.substring(ix - 3, ix + 3);
-					throw new Error("No input char at index" + index(ix) + ": " + error(err));
+					const err1 = str.substring(ix - err_length, ix);
+					const err2 = str.substring(ix + 1, ix + err_length);
+					throw new Error("No input char at index " + index(ix) + " : " + err1 + error(str[ix]) + err2);
 				}
+				break;
+			case "[":
+				if (tape[tpointer] === 0) isLoopFail = true;
+				else brackets.push(ix);
+				break;
+			case "]":
+				const temp = brackets.pop() - 1;
+				if (tape[tpointer] !== 0) ix = temp;
+				break;
 		}
 		ix++;
 	}
+	return tape;
 }
 
-brainfuck("+++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++.<.");
+brainfuck("+++++++++++++++++++++++++++++++++++.>[-]<[->+<]>---.>[-]<[->+<]>+++.");
+console.log();
+brainfuck(",.>,.>,.", { input: "hey", size: 4 });
+console.log();
+brainfuck("++++++++++[>+++++++>++++++++>+++<<<-]>+.--..++++++.>+++.>++.<<-----.>----.+++.>.<<+.--..++++++.>+.");
+console.log();
+brainfuck("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", {
+	size: 7,
+});
